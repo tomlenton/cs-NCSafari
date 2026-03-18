6de9f25014e355bce5caad7cfd0a143f1b3ab07f
tomlenton <twlenton@gmail.com>
2026-03-18 15:25:54 +0000

    feat: Add CompareWeight

---
+++ Animal/CompareWeight.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCSafari
+{
+    public class CompareWeight : IComparer<Animals>
+    {
+        public bool Descending;
+
+        public CompareWeight(bool descending)
+        {
+            Descending = descending;
+        }
+
+        public int Compare(Animals A, Animals B)
+        {
+            // Check nulls just in case
+            if (A == null && B == null) return 0;
+            if (A == null) return -1;
+            if (B == null) return 1;
+
+            if (A.WeightInKG > B.WeightInKG)
+            {
+                if (Descending)
+                {
+                    return -1; // the heaviest animal comes earlier in the list as -1 means A comes before B
+                }
+                else
+                {
+                    return 1;  // the biggest animal goes last in the lost as 1 means A comes after B
+                }
+            }
+            else if (A.WeightInKG < B.WeightInKG)
+            {
+                if (Descending)
+                {
+                    return 1;  // smaller last
+                }
+                else
+                {
+                    return -1; //biggest last
+                }
+            }
+            else
+            {
+                return 0; // weights are equal
+            }
+        }
+    }
+}
--- NCSafariZOO/Program.cs
+++ NCSafariZOO/Program.cs
@@ -5,23 +5,42 @@ internal class Program
 {
     public static void Main()
     {
-        Tiger fluffy = new Tiger(80);
-        Penguin penny = new Penguin(20.4f);
-        Goose jeff = new Goose(3);
-        Bat betty = new Bat(0.7f);
+        //Tiger fluffy = new Tiger(80);
+        //Penguin penny = new Penguin(20.4f);
+        //Goose jeff = new Goose(3);
+        //Bat betty = new Bat(0.7f);
 
-        Sky sky = new Sky();
-        sky.AddAnimalInEnvironment(betty);
-        sky.AddAnimalInEnvironment(jeff);
+        //Sky sky = new Sky();
+        //sky.AddAnimalInEnvironment(betty);
+        //sky.AddAnimalInEnvironment(jeff);
 
-        sky.CheckAnimalsInEnvironment();
+        //sky.CheckAnimalsInEnvironment();
 
-        HuntingGround hunting = new HuntingGround();
-        hunting.AddAnimalInEnvironment(betty);
-        hunting.AddAnimalInEnvironment(fluffy);
-        hunting.AddAnimalInEnvironment(penny);
+        //HuntingGround hunting = new HuntingGround();
+        //hunting.AddAnimalInEnvironment(betty);
+        //hunting.AddAnimalInEnvironment(fluffy);
+        //hunting.AddAnimalInEnvironment(penny);
 
-        hunting.CheckAnimalsInEnvironment();
+        //hunting.CheckAnimalsInEnvironment();
+
+        List<Animals> animalsToSee = new List<Animals>
+        {
+             new Tiger(70),
+             new Goose(5.2f),
+             new Bat(0.9f),
+             new Goose(4),
+             new Tiger(90),
+             new Penguin(15.8f)
+        };
+        // Should sort ascending
+        animalsToSee.Sort(new CompareWeight(false));
+
+        animalsToSee.ForEach(animal => Console.WriteLine($" {animal.GetType().Name} - {animal.WeightInKG}kg"));
+    
+    // Should sort descending
+    animalsToSee.Sort(new CompareWeight(true));
+
+        animalsToSee.ForEach(animal => Console.WriteLine($" {animal.GetType().Name} - {animal.WeightInKG}kg"));
     }
 }
 
